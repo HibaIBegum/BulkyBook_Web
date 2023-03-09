666076f76b5e06716eb29c481149bee0b3ccf825
hibaiqbalbegum <hib4@njit.edu>
2023-03-09 01:56:42 -0500

    Add

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objectCategory = _db.Categories;
+            return View(objectCategory);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Displayorder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id ==null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFound = _db.Categories.Find(id);
+            if(categoryFound == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFound);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Displayorder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited successfully";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFound = _db.Categories.Find(id);
+            if (categoryFound == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFound);
+        }
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var categoryFound = _db.Categories.Find(id);
+            if (categoryFound == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryFound);
+                _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+            
+            return View(categoryFound);
+        }
+    }
+}
+
